d9c9ff1c1efe5258d10dd613e15ef25a72b3d49d
Tanner <tannersbrown@gmail.com>
2022-07-13 13:19:56 -0700

    Working locally. Still needs routes etc

--- index.js
+++ index.js
@@ -3,43 +3,42 @@ const express = require('express');
 //Create a new instance of express
 const app = express();
 
+
+
+
+
 const bodyParser = require("body-parser");
 //This allows parsing of the body of POST requests, that are encoded in JSON
 app.use(bodyParser.json());
 
 //pg-promise is a postgres library that uses javascript promises
-const pgp = require('pg-promise')();
+//const pgp = require('pg-promise')();
 //We have to set ssl usage to true for Heroku to accept our connection
-pgp.pg.defaults.ssl = true;
+//pgp.pg.defaults.ssl = true;
+
+
 
 //Create connection to Heroku Database
-let db = pgp( process.env.DATABASE_URL);
-
-if(!db) {
-   console.log("SHAME! Follow the intructions and set your DATABASE_URL correctly");
-   process.exit(1);
-}
+let db = require('./util/sql_conn');
 
 
 
 
 
 /*
- * Return HTML for the / end point. 
+ * Return HTML for the / end point.
  * This is a nice location to document your web service API
- * Create a web page in HTML/CSS and have this end point return it. 
+ * Create a web page in HTML/CSS and have this end point return it.
  * Look up the node module 'fs' ex: require('fs');
  */
 app.get("/", (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    for (i = 1; i < 7; i++) {
-        //write a response to the client
-        res.write('<h' + i + ' style="color:green">Hello World!</h' + i + '>');
-    }
+    //write a response to the client
+    res.write('<h' + 1 + ' style="color:blue">APP IS ONLINE</h' + 1 + '>');
     res.end(); //end the response
 });
 
-/* 
+/*
 * Heroku will assign a port you can use via the 'PORT' environment variable
 * To accesss an environment variable, use process.env.<ENV>
 * If there isn't an environment variable, process.env.PORT will be null (or undefined)
@@ -47,8 +46,8 @@ app.get("/", (req, res) => {
 * In this case, we assign the port to be 5000 if the PORT variable isn't set
 * You can consider 'let port = process.env.PORT || 5000' to be equivalent to:
 * let port; = process.env.PORT;
-* if(port == null) {port = 5000} 
-*/ 
+* if(port == null) {port = 5000}
+*/
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server up and running on port: " + (process.env.PORT || 5000));
 });
---
+++ util/defs.js
@@ -0,0 +1,28 @@
+/**
+ * @Author Tanner Brown
+ * @Type {String}
+ *
+ * Holds constant var names to prevent misspellings
+ */
+
+const EPISODE_ID = 'episodeId';
+const START_DATE = 'startDate';
+const END_DATE = 'endDate';
+const DURATION = 'duration';
+const INTERVAL = 'interval';
+const HEARTRATE_DATA = 'heartRates';
+const SOUND_DATA = 'soundDbs'
+const RESTING_HR = 'avgRestingHr';
+const WALKING_HR = 'avgWalkingHr';
+const STEPS = 'steps';
+const RECENT_HRV = 'recentHRV';
+const USER_ID = 'userId';
+const DIAGNOSIS = 'diagnosis'
+const AGE = 'age'
+
+JSON_CONSTANTS = {EPISODE_ID, START_DATE, END_DATE, DURATION, INTERVAL, HEARTRATE_DATA, SOUND_DATA, RESTING_HR,
+    RECENT_HRV, WALKING_HR, STEPS, USER_ID, DIAGNOSIS, AGE}
+
+module.exports = {
+    JSON_CONSTANTS
+};
